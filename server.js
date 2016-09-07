3fa4b4178b21912ff6d08b68111440c118aed59e
Aidan VanLeuven <aidanvanleuven@gmail.com>
2016-09-07 15:11:47 -0600

    Add local IP and port to console output

--- server.js
+++ server.js
@@ -45,5 +45,20 @@ io.on('connection', function(socket){
   	});
 });
 
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+console.log(addresses + ":" + "3000");
+
 //Listen on port...
 server.listen(3000);
